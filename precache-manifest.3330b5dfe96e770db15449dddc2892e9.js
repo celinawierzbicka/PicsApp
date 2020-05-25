de0701d291d69abf1b1988afac43b64ffcf98adf
Celina Wierzbicka <celinawierzbicka@gmail.com>
2020-05-25 09:57:00 +0200

    Updates

---
+++ precache-manifest.3330b5dfe96e770db15449dddc2892e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5ea4bc66e674b1557b5d6e22c6344b",
+    "url": "/PicsApp/index.html"
+  },
+  {
+    "revision": "5b6a2c6b765ba8841e6b",
+    "url": "/PicsApp/static/css/main.84a9c081.chunk.css"
+  },
+  {
+    "revision": "71752e1f254d7fdddf9d",
+    "url": "/PicsApp/static/js/2.3f040f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PicsApp/static/js/2.3f040f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6a2c6b765ba8841e6b",
+    "url": "/PicsApp/static/js/main.6fccc587.chunk.js"
+  },
+  {
+    "revision": "b1afc086fb4135785546",
+    "url": "/PicsApp/static/js/runtime-main.2b57fb78.js"
+  }
+]);
